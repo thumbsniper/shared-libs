fc92d9cbba84b58857220a43c9700cac08a96c43
Thomas Schulte <thomas@cupracer.de>
2015-10-27 11:15:46 +0100

    Update Image.php

--- src/lib/thumbsniper/shared/Image.php
+++ src/lib/thumbsniper/shared/Image.php
@@ -1,12 +1,20 @@
 <?php
 
 /*
-* This file is part of CupRacer/thumbsniper-shared.
-*
-* (c) 2015 Thomas Schulte
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
+ * Copyright (C) 2015  Thomas Schulte <thomas@cupracer.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 namespace ThumbSniper\shared;
