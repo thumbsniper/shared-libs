fa6117a6687ce790335952eb90c10f0f5c66061c
Thomas Schulte <thomas@cupracer.de>
2016-02-01 20:12:46 +0100

    add height attribute to Image class remove minimum-stability from composer.json

--- lib/thumbsniper/shared/Image.php
+++ lib/thumbsniper/shared/Image.php
@@ -34,6 +34,9 @@ class Image
 	/** @var int */
     private $width;
 
+	/** @var int */
+	private $height;
+
 	/** @var string */
     private $effect;
 
@@ -132,6 +135,22 @@ class Image
 	{
 		$this->width = $width;
 	}
+
+    /**
+     * @return int
+     */
+    public function getHeight()
+    {
+        return $this->height;
+    }
+
+    /**
+     * @param int $height
+     */
+    public function setHeight($height)
+    {
+        $this->height = $height;
+    }
 
 	/**
 	 * @return string
